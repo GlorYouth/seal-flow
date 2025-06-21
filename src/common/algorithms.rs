d4429732933dd02f31e8af85c5dfd00936082dc6
韶光 <admin@gloryouth.com>
2025-06-21 23:17:36 +0800

    Add support for AES-128-GCM and new asymmetric algorithms (RSA-4096, Kyber variants)

--- src/common/algorithms.rs
+++ src/common/algorithms.rs
@@ -3,6 +3,7 @@ use bincode::{Decode, Encode};
 /// 对称加密算法枚举
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Decode, Encode)]
 pub enum SymmetricAlgorithm {
+    Aes128Gcm,
     Aes256Gcm,
 }
 
@@ -10,4 +11,8 @@ pub enum SymmetricAlgorithm {
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Decode, Encode)]
 pub enum AsymmetricAlgorithm {
     Rsa2048,
+    Rsa4096,
+    Kyber512,
+    Kyber768,
+    Kyber1024,
 }
